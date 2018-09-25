677362a6d73e5d213d7c0dd048663505a5434a50
Franke Telgenhof <franke.telgenhof@postcodeloterij.nl>
2018-09-25 13:42:59 +0200

    created landing page

--- iban_webservice.py
+++ iban_webservice.py
@@ -3,6 +3,10 @@ from iban import Iban
 
 app = Flask(__name__)
 
+@app.route('/', methods=['GET'])
+def mainPage():
+    return '<a href="/getIban">Get IBAN webservice</a>'
+
 @app.route('/getIban', methods=['GET'])
 def getIban():
     newIban = Iban.generateIban()
@@ -13,4 +17,3 @@ def runApp():
 
 if __name__ == '__main__':
     runApp()
-    
