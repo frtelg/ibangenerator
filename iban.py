2ce8106c39a467c5aba6aa3dbefcdb56e15a9a4e
Franke Telgenhof <franke.telgenhof@trivento.nl>
2018-11-14 23:25:16 +0100

    placed all methods inside the Iban class

--- iban.py
+++ iban.py
@@ -5,86 +5,93 @@
 import random
 import pyperclip
 
-def elevenTest(bankNumber):
-    bankNumber = int(bankNumber)
-    if bankNumber % 11 == 0:
-        return 1
-    else: 
-        return 0
-
-def stringToInt(bankString):
-    bankArray = list(bankString)
-    i = 0
-    for x in bankArray:
-        if x.isdigit() == False:
-            bankArray[i] = ord(x) - 55
-        i = i + 1
-
-    i = 0
-    for x in bankArray:
-        bankArray[i] = str(bankArray[i])
-        i = i + 1
-
-    bankString = ''.join(bankArray)
-    return bankString
-
-def convertIbanToInt(bankString):
-    controlNumber = bankString[4] + bankString[5]
-    controlInt = int(controlNumber)
-
-    if controlInt < 2 or controlInt > 98: 
-        return 0
-    else: 
-        firstPart = bankString[0:6]
-        secondPart = bankString[6:]
-        mod97ControlNumber = secondPart + firstPart
-        mod97ControlNumber = int(mod97ControlNumber)
-        return mod97ControlNumber
-
-def defineControlNumber(bank,bankAccount):
-    bank = str(stringToInt(bank))
-    bankAccountString = str(bankAccount)
-    controlNumberInput = bankAccountString + bank
-    controlNumberInput = int(controlNumberInput)
-    controlNumberMod = controlNumberInput % 97
-    controlNumber = 98 - controlNumberMod
-    if controlNumber < 10:
-        controlNumber = str(controlNumber)
-        controlNumber = '0' + controlNumber
-    else: 
-        controlNumber = str(controlNumber)
-    return controlNumber
-
-def mod97(mod97ControlNumber):
-    checkSum = mod97ControlNumber % 97
-    if checkSum == 1:
-        return True
-    else: 
-        return False
-
-def randomBank():
-    bankArray = ['INGB', 'SNSB', 'RABO', 'ABNA', 'TRIO', 'ASNB', 'DEUT', 'AEGO', 'BICK', 'KNAB']
-    randomBank = bankArray[random.randint(0,9)]
-    return randomBank
-
-def generateBankAccount():
-    test = 0
-
-    while test == 0:
-        accountArray = []
-
-        for _ in range(10):
-            accountArray.append(str(random.randint(0,9)))
-        
-        generatedBankAccount = ''.join(accountArray)
-        test = elevenTest(generatedBankAccount)
-
-    bankString = generatedBankAccount
-    return bankString
-
 class Iban:
     def __init__(self, iban):
         self.iban = iban
+
+    @staticmethod
+    def elevenTest(bankNumber):
+        bankNumber = int(bankNumber)
+        if bankNumber % 11 == 0:
+            return 1
+        else: 
+            return 0
+
+    @staticmethod
+    def stringToInt(bankString):
+        bankArray = list(bankString)
+        i = 0
+        for x in bankArray:
+            if x.isdigit() == False:
+                bankArray[i] = ord(x) - 55
+            i = i + 1
+
+        i = 0
+        for x in bankArray:
+            bankArray[i] = str(bankArray[i])
+            i = i + 1
+
+        bankString = ''.join(bankArray)
+        return bankString
+
+    @staticmethod
+    def convertIbanToInt(bankString):
+        controlNumber = bankString[4] + bankString[5]
+        controlInt = int(controlNumber)
+
+        if controlInt < 2 or controlInt > 98: 
+            return 0
+        else: 
+            firstPart = bankString[0:6]
+            secondPart = bankString[6:]
+            mod97ControlNumber = secondPart + firstPart
+            mod97ControlNumber = int(mod97ControlNumber)
+            return mod97ControlNumber
+
+    @staticmethod
+    def defineControlNumber(bank,bankAccount):
+        bank = str(Iban.stringToInt(bank))
+        bankAccountString = str(bankAccount)
+        controlNumberInput = bankAccountString + bank
+        controlNumberInput = int(controlNumberInput)
+        controlNumberMod = controlNumberInput % 97
+        controlNumber = 98 - controlNumberMod
+        if controlNumber < 10:
+            controlNumber = str(controlNumber)
+            controlNumber = '0' + controlNumber
+        else: 
+            controlNumber = str(controlNumber)
+        return controlNumber
+
+    @staticmethod
+    def mod97(mod97ControlNumber):
+        checkSum = mod97ControlNumber % 97
+        if checkSum == 1:
+            return True
+        else: 
+            return False
+
+    @staticmethod
+    def randomBank():
+        bankArray = ['INGB', 'SNSB', 'RABO', 'ABNA', 'TRIO', 'ASNB', 'DEUT', 'AEGO', 'BICK', 'KNAB']
+        randomBank = bankArray[random.randint(0,9)]
+        return randomBank
+
+    @staticmethod
+    def generateBankAccount():
+        test = 0
+
+        while test == 0:
+            accountArray = []
+
+            for _ in range(10):
+                accountArray.append(str(random.randint(0,9)))
+            
+            generatedBankAccount = ''.join(accountArray)
+            test = Iban.elevenTest(generatedBankAccount)
+
+        bankString = generatedBankAccount
+        return bankString
 
     @classmethod
     def generateIban(cls): 
@@ -92,13 +99,13 @@ class Iban:
 
         while outcome == False:
             country = 'NL'
-            bank = randomBank()
-            bankAccount = generateBankAccount()
-            controlNumber = defineControlNumber(bank,bankAccount)
+            bank = Iban.randomBank()
+            bankAccount = Iban.generateBankAccount()
+            controlNumber = Iban.defineControlNumber(bank,bankAccount)
             generatedIban = country + controlNumber + bank + bankAccount
-            intIban = stringToInt(generatedIban)
-            intIban = convertIbanToInt(intIban)
-            outcome = mod97(intIban)
+            intIban = Iban.stringToInt(generatedIban)
+            intIban = Iban.convertIbanToInt(intIban)
+            outcome = Iban.mod97(intIban)
 
         return cls(generatedIban)
 
